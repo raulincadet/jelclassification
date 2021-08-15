8de830f6de8ed4effc789303915d55b298c175d7
Raulin L. Cadet <cadetraulin@gmail.com>
2021-08-14 23:59:12 -0400

    Correction

--- R/jel_keywords_count.R
+++ R/jel_keywords_count.R
@@ -2,7 +2,7 @@
 jel_keywords_count<-function(){
   require(tidyverse)
 
-  load("data/df_jel.rda")
+  data("df_jel")
   as_tibble(data.frame(df_jel))%>%
   group_by(Code,Theme)%>%
   count()%>%na.omit()
