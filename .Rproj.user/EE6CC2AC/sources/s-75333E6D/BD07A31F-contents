848399d11566a18d03ccdad0168c52ad8e43ffc9
Raulin L. Cadet <cadetraulin@gmail.com>
2021-08-15 00:29:31 -0400

    use internatl data

--- .Rproj.user/EE6CC2AC/sources/s-75333E6D/BD07A31F-contents
+++ .Rproj.user/EE6CC2AC/sources/s-75333E6D/BD07A31F-contents
@@ -2,7 +2,7 @@
 jel_keywords_count<-function(){
   require(tidyverse)
 
-  data("df_jel")
+ # data("df_jel")
   as_tibble(data.frame(df_jel))%>%
   group_by(Code,Theme)%>%
   count()%>%na.omit()
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,10 +5,9 @@
 library(tidyverse)
 df_jel<-readxl::read_excel("data-raw/jel.xlsx")
 df_jel<-na.omit(df_jel)
-#usethis::use_data(df_jel,overwrite = TRUE,internal = T)
+usethis::use_data(df_jel,internal = TRUE)#,overwrite = TRUE,internal = T)
 #save(df_jel,file="df_jel.RData")
 #usethis::use_data(df_jel, internal = F)
-save(df_jel, file = "data/df_jel.rda")#;close(con)
-
+#save(df_jel, file = "data/df_jel.rda")#;close(con)
 
 
